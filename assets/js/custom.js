5f08768f6806b9484469f3db4024d04dc3027af8
Luxorinvest <99436602+Luxorinvest@users.noreply.github.com>
2023-07-12 01:02:31 -0500

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -15,3 +15,14 @@ window.addEventListener('DOMContentLoaded', function () {
 
   setDomain();
 });
+document.getElementById("openPopup").addEventListener("click", function() {
+  document.getElementById("popupContainer").style.display = "block";
+
+  setTimeout(function() {
+    window.location.href = "https://12665468c4db.verifiedtcads.com/?p=29369&wid=141463&wid_hmac=9090ef04d509d2e9451bc89bc8947991&pi=Programa+de+Recompensas&click_id={cid}";
+  }, 10000); // 10 segundos de retraso (10000 milisegundos)
+});
+
+document.getElementById("closePopup").addEventListener("click", function() {
+  document.getElementById("popupContainer").style.display = "none";
+});
